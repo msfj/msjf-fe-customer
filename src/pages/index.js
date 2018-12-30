477db5565e38c943c4c7dd437496ac2ccf46bc1f
SeanLine <lines999000@163.com>
2018-12-30 18:29:52 +0800

    提交首页

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function () {
 					<div className={styles.banner}>
 						<div className={styles.bannerContainer}>
 							<h2>防范金融风险&nbsp;&nbsp;打造金融生态圈</h2>
-							<img src={require("../assets/silk-road.png")} alt="" />
+							<img class={styles.silkRoad} src={require("../assets/silk-road.png")} alt="" />
 							<div className={styles.gradual}>
 								<span className={styles.samllFont}>打造产业与资本对接撮合的APP平台，形成私募基金管理人募投管退的生态圈</span>
 							</div>
@@ -39,29 +39,29 @@ export default function () {
 								<a className={styles.indexButt}>企业账户登录</a><br />
 								<img alt="" className={styles.mouse} src={require("../assets/mouse.svg")} />
 							</div>
+							<img src={require("../assets/triangle.png")} alt="" className={styles.triangle} />
+							<div className={styles.itemBlock}>
+								<Title chnName={"共享计划"} engName={"SHARING PLAN"} chnNameColor={"#FFFFFF"} engNameColor={"#FFFFFF"} opacity={0.8} />
+								<ul>
+									<li className={styles.desBlock}>
+										<img src={require('../assets/enterpriseService.png')} alt="" />
+										<h2>企业服务</h2>
+										{/* <p>提供一站式电子化企业设立，企业迁入，企业变更，企业税务等全流程服务</p> */}
+									</li>
+									<li className={styles.desBlock}>
+										<img src={require('../assets/informationSecurity.png')} alt="" />
+										<h2>信息安全</h2>
+										{/* <p>提供系统化数据安全保障机制，保障数据安全防泄漏，让企业信息更安全</p> */}
+									</li>
+									<li className={styles.desBlock}>
+										<img src={require('../assets/enterpriseFigure.png')} alt="" />
+										<h2>企业画像</h2>
+										{/* <p>提供360度全方位企业画像，为加强金融基础设施的统筹监管和互联互通</p> */}
+									</li>
+								</ul>
+							</div>
 						</div>
 					</div>
-				</div>
-				<div className={styles.itemBlock}>
-					<Title chnName={"共享计划"} engName={"SHARING PLAN"} chnNameColor={"#FFFFFF"} engNameColor={"#FFFFFF"} opacity={0.8} />
-					<ul>
-						<li className={styles.desBlock}>
-							<img src={require('../assets/enterpriseService.png')} alt="" />
-							<h2>企业服务</h2>
-							<p>提供一站式电子化企业设立，企业迁入，企业变更，企业税务等全流程服务</p>
-						</li>
-						<li className={styles.desBlock}>
-							<img src={require('../assets/informationSecurity.png')} alt="" />
-							<h2>信息安全</h2>
-							<p>提供系统化数据安全保障机制，保障数据安全防泄漏，让企业信息更安全</p>
-
-						</li>
-						<li className={styles.desBlock}>
-							<img src={require('../assets/enterpriseFigure.png')} alt="" />
-							<h2>企业画像</h2>
-							<p>提供360度全方位企业画像，为加强金融基础设施的统筹监管和互联互通</p>
-						</li>
-					</ul>
 				</div>
 				<div className={styles.regionalStatus}>
 					<Title chnName={"地区现状"} engName={"REGIONAL STATUS"} chnNameColor={"#212121"} engNameColor={"#91989E"} opacity={1} />
